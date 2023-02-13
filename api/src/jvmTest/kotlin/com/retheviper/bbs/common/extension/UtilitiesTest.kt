d5347a10fef1cd65c253dc4e18f7bc6f818df056
retheviper <retheviper@gmail.com>
2023-02-13 12:09:07 +0900

    fix

--- api/src/jvmTest/kotlin/com/retheviper/bbs/common/extension/UtilitiesTest.kt
+++ api/src/jvmTest/kotlin/com/retheviper/bbs/common/extension/UtilitiesTest.kt
@@ -1,13 +1,13 @@
 package com.retheviper.bbs.common.extension
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
 
 class UtilitiesTest : FreeSpec({
 
     "Hash Test" {
         val password = "password"
         val hashed = password.toHashedString()
-        println(hashed)
-        println(password verifyWith hashed)
+        (password verifyWith hashed) shouldBe true
     }
 })
